345912caab02bc95502a7ee863887ce36e645210
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-03 16:03:03 +0700

    fix

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -3,7 +3,7 @@ import Tool from './pages/Tool';
 
 Nova.booting((app, store) => {
     app.use(ToggleButton);
-    app.component('menu-builder', require('./components/BuilderResourceTool'));
+    app.component('menu-builder', require('./components/BuilderResourceTool').default);
 
     Nova.inertia('MenuBuilder', Tool);
 });
