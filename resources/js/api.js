2a8c5bbd1500a7b36867f3aea6db8f3dff423aa2
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-11 23:51:55 +0700

    fx

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,42 +1,42 @@
 export default {
     getItems(menu) {
-        return window.axios
+        return this.axios
             .post('/nova-vendor/menu-builder/items', {
                 menu: menu,
             })
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 
     saveItems(menu, menuItems) {
-        return window.axios
+        return this.axios
             .post('/nova-vendor/menu-builder/save-items', {
                 menu: menu,
                 items: menuItems,
             })
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 
     create(menuItem) {
-        return window.axios
+        return this.axios
             .post('/nova-vendor/menu-builder/new-item', menuItem)
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 
     edit(menu) {
-        return window.axios
+        return this.axios
             .get('/nova-vendor/menu-builder/edit/' + menu)
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 
     update(menuItemId, menuItem) {
-        return window.axios
+        return this.axios
             .post('/nova-vendor/menu-builder/update/' + menuItemId, menuItem)
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 
     destroy(menuItemId) {
-        return window.axios
+        return this.axios
             .post('/nova-vendor/menu-builder/destroy/' + menuItemId)
-            .then(response => response.data);
+            .then((response) => response.data);
     },
 };
--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,9 +1,12 @@
 import ToggleButton from '@vueform/toggle';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import Tool from './pages/Tool';
 
 Nova.booting((Vue, store) => {
     Vue.use(ToggleButton);
-    Vue.component('menu-builder', require('./components/BuilderResourceTool').default);
+    Vue.use(VueAxios, axios);
+    Vue.component('builder-resource-tool', require('./components/BuilderResourceTool').default);
 
     Nova.inertia('MenuBuilder', Tool);
 });
